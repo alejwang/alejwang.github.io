cab83552c00f08d7d4dcd64e3c178463ff676928
alejwang <github@alej.wang>
2018-03-28 13:11:22 -0400

    Rewrite the preloader.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,24 @@
-/* ===================================================================
- * Infinity - Main JS
- *
- * ------------------------------------------------------------------- */
+"use strict";
 
-(function($) {
+var ssPreloader = function() {
 
-	"use strict";
+  $(window).on('load', function() {
 
-	var cfg = {
-		defAnimation   : "fadeInUp",    // default css animation
-		scrollDuration : 800,           // smoothscroll duration
-		mailChimpURL   : 'http://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e65110b38d'
-	},
+    // force page scroll position to top at page refresh
+    $('html, body').animate({
+      scrollTop: 0
+    }, 'normal');
 
-	$WIN = $(window);
+    // will first fade out the loading animation
+    $("#preloader").fadeOut("slow", function() {
 
+      // will fade out the whole DIV that covers the website.
+      $("#preloader-container").delay(300).fadeOut("slow");
 
-   // Add the User Agent to the <html>
-   // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
-	var doc = document.documentElement;
-	doc.setAttribute('data-useragent', navigator.userAgent);
+    });
+  });
+};
 
-
-	/* Preloader
-	 * -------------------------------------------------- */
-	var ssPreloader = function() {
-
-		$WIN.on('load', function() {
-
-			// force page scroll position to top at page refresh
-			$('html, body').animate({ scrollTop: 0 }, 'normal');
-
-	      // will first fade out the loading animation
-	    	$("#loader").fadeOut("slow", function(){
-
-	        // will fade out the whole DIV that covers the website.
-	        $("#preloader").delay(300).fadeOut("slow");
-
-	      });
-	  	});
-	};
-
-
-
-
-
-  /* Initialize
-	* ------------------------------------------------------ */
-	(function ssInit() {
-
-		ssPreloader();
-
-	})();
-
-
-})(jQuery);
+(function ssInit() {
+  ssPreloader();
+})();
